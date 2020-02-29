a15199a13bc8cd03e66a73a6b82194ef57d3cddc
Vinicius Santos <viniciusmsos@gmail.com>
2020-02-29 17:03:47 -0300

    #1 Solved using map Added test cases

--- 001-feb29-2020.go
+++ 001-feb29-2020.go
@@ -1,22 +1,37 @@
 package dailycodeproblem
 
-// DoesItSumTo problem:
 // Good morning! Here's your coding interview problem for today.
 // This problem was recently asked by Google.
 // Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 // For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 // Bonus: Can you do this in one pass?
-func DoesItSumTo(k int, arr []int) bool {
-	for ii, i := range arr {
-		for ij, j := range arr {
+
+func doesItSumTo(k int, arr []int) bool {
+	for ii, vi := range arr {
+		for ij, vj := range arr {
 			if ii == ij {
 				continue
 			}
 
-			if i+j == k {
+			if vi+vj == k {
 				return true
 			}
 		}
+	}
+	return false
+}
+
+func mapDoesItSumTo(k int, arr []int) bool {
+	m := make(map[int]bool)
+
+	for _, v := range arr {
+		need := k - v
+
+		if m[need] == true {
+			return true
+		}
+
+		m[v] = true
 	}
 	return false
 }
--- 001-feb29-2020_test.go
+++ 001-feb29-2020_test.go
@@ -16,11 +16,19 @@ func TestDoesItSumTo(t *testing.T) {
 		{"2", args{k: 55, arr: []int{10, 15, 3, 7}}, false},
 		{"3", args{k: -12, arr: []int{-8, -6, -4, 4}}, true},
 		{"4", args{k: -34, arr: []int{0, 0, 77, 12}}, false},
+		{"5", args{k: 0, arr: []int{0, 0, 77, 12}}, true},
+		{"6", args{k: 0, arr: []int{0, 77, 12}}, false},
+		{"7", args{k: 0, arr: []int{-128, 128, 77, 12}}, true},
+		{"8", args{k: 0, arr: []int{-128}}, false},
+		{"9", args{k: 0, arr: []int{}}, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := DoesItSumTo(tt.args.k, tt.args.arr); got != tt.want {
-				t.Errorf("DoesItSumTo() = %v, want %v", got, tt.want)
+			if got := doesItSumTo(tt.args.k, tt.args.arr); got != tt.want {
+				t.Errorf("doesItSumTo() = %v, want %v", got, tt.want)
+			}
+			if got := mapDoesItSumTo(tt.args.k, tt.args.arr); got != tt.want {
+				t.Errorf("mapDoesItSumTo() = %v, want %v", got, tt.want)
 			}
 		})
 	}
