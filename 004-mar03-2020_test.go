5b3dc22a78e4b306e00ca13e3fe042eca3578fac
Vinicius Santos <viniciusmsos@gmail.com>
2020-03-04 11:28:16 -0300

    Add tests to #4, current solution is wrong

--- 004-mar03-2020_test.go
+++ 004-mar03-2020_test.go
@@ -22,6 +22,19 @@ func Test_firstMissingPositiveInteger(t *testing.T) {
 		{"9", args{arr: []int{44, 33, 3, 1}}, 2},
 		{"10", args{arr: []int{5, 4, -3, -2, 1}}, 2},
 		{"11", args{arr: []int{6, 5, 4, -3, -2, 1}}, 2},
+		{"12", args{arr: []int{6, 1}}, 2},
+		{"13", args{arr: []int{1}}, 2},
+		{"14", args{arr: []int{2}}, 1},
+		{"15", args{arr: []int{-6, -5, 4, -3, -2, -1}}, 1},
+		{"16", args{arr: []int{4, 2, 1}}, 3},
+		{"17", args{arr: []int{4, 1, 2}}, 3},
+		{"18", args{arr: []int{2, 4, 1}}, 3},
+		{"19", args{arr: []int{2, 1, 4}}, 3},
+		{"20", args{arr: []int{1, 2, 4}}, 3},
+		{"21", args{arr: []int{1, 4, 2}}, 3},
+		{"22", args{arr: []int{}}, 1},
+		{"23", args{arr: []int{0, 3, 1, 2, 4}}, 5},
+		{"24", args{arr: []int{5, 2, 4, 0, 3, 1}}, 6},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
